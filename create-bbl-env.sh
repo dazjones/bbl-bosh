4e94bfd4d75f10a44b0cce3905b7a1f278bae006
Darren Jones <darren@darrenjonesdesign.co.uk>
2018-01-26 16:04:03 +0000

    Removed references to gcloud

--- create-bbl-env.sh
+++ create-bbl-env.sh
@@ -7,27 +7,12 @@ SERVICE_ACCOUNT_KEY_FILE="${BBL_STATE_PATH}/${SERVICE_ACCOUNT}.key.json"
 
 export BBL_IAAS=gcp
 export BBL_ENV_NAME=${BBL_ENVIRONMENT}
-export BBL_GCP_REGION=$(gcloud config get-value compute/region 2>/dev/null)
+export BBL_GCP_REGION=${GCP_REGION}
 
 if [ ! -d ${BBL_STATE_PATH} ]
 then
     mkdir -p ${BBL_STATE_PATH}
 fi
-
-if [[ $(gcloud iam --project ${GCP_PROJECT} service-accounts list | grep -c "${SERVICE_ACCOUNT}") -eq 0 ]]
-then
-    gcloud iam --project ${GCP_PROJECT} service-accounts create ${SERVICE_ACCOUNT} \
-               --display-name=${SERVICE_ACCOUNT}
-fi
-
-if [ ! -f ${SERVICE_ACCOUNT_KEY_FILE} ]
-then
-    gcloud iam --project ${GCP_PROJECT} service-accounts keys create --iam-account="${SERVICE_ACCOUNT}@${GCP_PROJECT}.iam.gserviceaccount.com" ${SERVICE_ACCOUNT_KEY_FILE}
-fi
-
-gcloud projects add-iam-policy-binding ${GCP_PROJECT} \
-    --member=serviceAccount:${SERVICE_ACCOUNT}@${GCP_PROJECT}.iam.gserviceaccount.com \
-    --role=roles/owner
 
 export BBL_GCP_SERVICE_ACCOUNT_KEY=$(cat ${SERVICE_ACCOUNT_KEY_FILE})
 

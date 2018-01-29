9e90c3b5d25c0b93f77da762733c5db2119f3825
Darren Jones <darren@darrenjonesdesign.co.uk>
2018-01-29 11:25:50 +0000

    removed gcloud references from destroy script

--- destroy-bbl-env.sh
+++ destroy-bbl-env.sh
@@ -1,4 +1,4 @@
-GCP_PROJECT=$(gcloud config get-value project 2>/dev/null)
+GCP_PROJECT=${GCP_PROJECT}
 BBL_ENVIRONMENT=bbl-${GCP_PROJECT}
 BASE_PATH=$(pwd)
 BBL_STATE_PATH=${BASE_PATH}/${BBL_ENVIRONMENT}
@@ -7,7 +7,7 @@ SERVICE_ACCOUNT_KEY_FILE="${BBL_STATE_PATH}/${SERVICE_ACCOUNT}.key.json"
 
 export BBL_IAAS=gcp
 export BBL_ENV_NAME=${BBL_ENVIRONMENT}
-export BBL_GCP_REGION=$(gcloud config get-value compute/region 2>/dev/null)
+export BBL_GCP_REGION=${GCP_REGION}
 export BBL_GCP_SERVICE_ACCOUNT_KEY=$(cat ${SERVICE_ACCOUNT_KEY_FILE})
 
 if [ ! -d ${BBL_STATE_PATH} ]
@@ -16,3 +16,4 @@ then
 else
     bbl destroy -s ${BBL_STATE_PATH} $*
 fi
+B

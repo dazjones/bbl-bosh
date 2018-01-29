a1da6ba96809ca849e8d7611bfaebe8217ebfb45
Darren Jones <darren@darrenjonesdesign.co.uk>
2018-01-29 15:26:09 +0000

    removed no-ip tagging

--- create-bbl-env.sh
+++ create-bbl-env.sh
@@ -18,9 +18,4 @@ export BBL_GCP_SERVICE_ACCOUNT_KEY=$(cat ${SERVICE_ACCOUNT_KEY_FILE})
 
 bbl plan -s ${BBL_STATE_PATH} $*
 
-# Remove director external IP and set director tags
-ENV_ID=$(grep env_id "${BBL_STATE_PATH}/vars/terraform.tfvars" | sed 's/^env_id="\(.*\)"$/\1/g')
-sed -i "s/^.*bosh-director-ephemeral-ip-ops.*$/  -v  tags=[${ENV_ID}-bosh-director,no-ip]/g" "${BBL_STATE_PATH}/create-director.sh"
-sed -i "s/^.*bosh-director-ephemeral-ip-ops.*$/  -v  tags=[${ENV_ID}-bosh-director,no-ip]/g" "${BBL_STATE_PATH}/delete-director.sh"
-
 bbl up -s ${BBL_STATE_PATH} $*
